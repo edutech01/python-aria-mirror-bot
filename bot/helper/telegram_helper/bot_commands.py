0ade3ec39b1e5cc009714fe9c259b801aa4d3b59
edutech01 <74646062+edutech01@users.noreply.github.com>
2020-11-18 13:46:25 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,22 +1,22 @@
 class _BotCommands:
     def __init__(self):
-        self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
-        self.UnzipMirrorCommand = 'unzipmirror'
-        self.TarMirrorCommand = 'tarmirror'
-        self.CancelMirror = 'cancel'
-        self.CancelAllCommand = 'cancelall'
-        self.ListCommand = 'list'
+        self.StartCommand = 'start01'
+        self.MirrorCommand = 'mirror01'
+        self.UnzipMirrorCommand = 'unzipmirror01'
+        self.TarMirrorCommand = 'tarmirror01'
+        self.CancelMirror = 'cancel01'
+        self.CancelAllCommand = 'cancelall01'
+        self.ListCommand = 'list01'
         self.StatusCommand = 'status'
         self.AuthorizeCommand = 'authorize'
         self.UnAuthorizeCommand = 'unauthorize'
         self.PingCommand = 'ping'
-        self.RestartCommand = 'restart'
+        self.RestartCommand = 'restart01'
         self.StatsCommand = 'stats'
-        self.HelpCommand = 'help'
-        self.LogCommand = 'log'
-        self.CloneCommand = "clone"
-        self.WatchCommand = 'watch'
-        self.TarWatchCommand = 'tarwatch'
+        self.HelpCommand = 'help001'
+        self.LogCommand = 'log001'
+        self.CloneCommand = "clone01"
+        self.WatchCommand = 'watch01'
+        self.TarWatchCommand = 'tarwatch01'
 
 BotCommands = _BotCommands()
